ffabc6e1877bcd92c1b34dcfd5aaed0ff9ce7592
MrShahrom <shahromeo777@gmail.com>
2025-03-03 10:55:27 +0500

    Added calculator

--- ITRunCourse/ITRunApp/Program.cs
+++ ITRunCourse/ITRunApp/Program.cs
@@ -1,6 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Enter ur name: ");
-string urName = Console.ReadLine();
-Console.WriteLine($"Hello, {urName} ");
+// Console.Write("Enter your name: ");
+// string urName = Console.ReadLine();
+// Console.WriteLine($"Hello, {urName} ");
 
+Console.WriteLine("Input firstNumber:");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Input secondNumber");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Input expression:");
+double result = 0.0;
+char value = char.Parse(Console.ReadLine());
+
+switch (value)
+{
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+}
+Console.WriteLine($"{num1} {value} {num2} = {result}");
