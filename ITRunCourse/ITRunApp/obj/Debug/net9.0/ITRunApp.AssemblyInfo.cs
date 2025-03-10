bff4eea2cc754a9f82fb4f43b10558527757c42f
MrShahrom <shahromeo777@gmail.com>
2025-03-10 06:04:03 +0500

    added class

--- ITRunCourse/ITRunApp/obj/Debug/net9.0/ITRunApp.AssemblyInfo.cs
+++ ITRunCourse/ITRunApp/obj/Debug/net9.0/ITRunApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITRunApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1d4983c9f4fb83f6ef8d5c8649eb5525e5acb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d98bcb90922f90a07ad7979b70890e68c1eaf36e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITRunApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITRunApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
